6c8a9232be8ff111a70314b771b7a55e8dbbb2cb
Aleksandre Nutsubidze <aleksandrenutsubidze@gmail.com>
2023-08-07 12:04:16 +0400

    add access controll allow origin

--- src/controllers/payment-controller.js
+++ src/controllers/payment-controller.js
@@ -6,6 +6,9 @@ const { createCheckoutSession } = require("../services/payment-services");
 
 router.post("/", async (req, res) => {
   try {
+    res.header("Access-Control-Allow-Origin", "*"); // Replace "*" with your client's origin URL for a more restricted setup.
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     const session = await createCheckoutSession(req.body.jwt);
     console.log(session);
     res.json({ session });
