267b1af6be56ba9bb57c57e712e724a7ba63f45a
Aleksandre Nutsubidze <aleksandrenutsubidze@gmail.com>
2023-08-13 16:05:59 +0400

    pagination with query params

--- src/controllers/websiteData-controller.js
+++ src/controllers/websiteData-controller.js
@@ -4,8 +4,13 @@ const router = express.Router();
 const WebsiteDatas = require("../schemas/SiteDatas");
 
 router.get("/", async (req, res) => {
+  const { page, limit } = req.query;
+  console.log(page, limit);
   try {
-    const data = await WebsiteDatas.find();
+    const data = await WebsiteDatas.find()
+      .skip((page - 1) * limit)
+      .limit(limit * 4 * page);
+
     if (!data)
       return res
         .status(403)
